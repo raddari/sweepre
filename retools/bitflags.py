c8893833686c2e44a6e74a600b994d871a82175c
raddari <12421643-raddari@users.noreply.gitlab.com>
2022-12-22 01:11:17 +1030

    feat: add bitflags python script

---
+++ retools/bitflags.py
@@ -0,0 +1,15 @@
+#!/bin/python3.11
+import sys
+
+def decompose(flags: int) -> list[int]:
+    binstr = bin(flags)[2:]
+    return [2 ** i for i, c in enumerate(reversed(binstr)) if c == '1']
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'usage: {sys.argv[0]} <hex_flags>')
+        sys.exit(0)
+
+    decomposed = decompose(int(sys.argv[1], 16))
+    print(' | '.join([f'0x{i:08X}' for i in reversed(decomposed)]))
+
