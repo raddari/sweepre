010192153bcf26330fd8e61f8702a96e8702952a
raddari <12421643-raddari@users.noreply.gitlab.com>
2022-12-22 01:11:42 +1030

    feat: add skeleton C program

---
+++ src/sweepre.cpp
@@ -0,0 +1,12 @@
+#include <Process.h>
+
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fwdReason, LPVOID lpvReserverd) {
+  switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+      break;
+    case DLL_PROCESS_DETACH:
+      break;
+  }
+  return TRUE;
+}
