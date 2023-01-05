ee6d7f838d54629e2067358ef1289773da7dad1b
raddari <harveywyatt@hotmail.com>
2023-01-05 22:58:46 +1030

    fix: remove unused dll export

--- src/dllhook/winmm_hook.h
+++ src/dllhook/winmm_hook.h
@@ -2,8 +2,6 @@
 
 #include <windows.h>
 
-#define DLLEXPORT __declspec(dllexport)
-
 
 #pragma comment(linker, "/export:PlaySoundW=_PlaySoundW@12")
-EXTERN_C DLLEXPORT BOOL WINAPI PlaySoundW(LPCWSTR pszSound, HMODULE hmod, DWORD fdwSound);
+EXTERN_C BOOL WINAPI PlaySoundW(LPCWSTR pszSound, HMODULE hmod, DWORD fdwSound);
