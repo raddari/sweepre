150230a86bd1fb36444f41b21d12f3c661b58cff
raddari <harveywyatt@hotmail.com>
2023-03-28 00:35:59 +1030

    tidy: remove example main

--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-
-auto APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) -> int {
-  return MessageBox(nullptr, L"Hello, world!", L"caption", 0);
-}
